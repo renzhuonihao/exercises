5a2101ca26c21afccae688411b05c2d844d5acac
任卓 <renzhuonihao@gmail.com>
2015-07-06 03:13:34 +0800

    changed

--- SICP/c1/11.scm
+++ SICP/c1/11.scm
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+;#reader(lib "htdp-advanced-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 ;;递归计算过程版本
 (define (f n)
   (if (< n 3)
@@ -29,3 +29,5 @@
                       c)
                (+ count 1)
                max-count)))
+
+(+ 1 2)
